87f327e73d75c1f6f915c38be2c6bb276a807994
osamazaki <osama.zaki.rasmy@gmail.com>
2019-12-23 22:53:47 +0200

    this is the my commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        a:1,
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    },
+    created: function () {
+        // `this` points to the vm instance
+        console.log('a is: ' + this.a)
+    }
+})
+
